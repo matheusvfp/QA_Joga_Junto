7ba39eae352f4a73c2e93a100a7843a5f8a7a176
Matheus Vinicius <matheusv83200@gmail.com>
2024-06-13 20:38:23 -0300

    aula1

---
+++ aula1.py
@@ -0,0 +1,8 @@
+sobrenome = "MARTINS"
+primeiro_nome = "PAULA"
+mes = "JANEIRO "
+valor = 500.00
+desconto = 10
+cupom = primeiro_nome+'É10'
+
+print(f"Olá,{primeiro_nome} {sobrenome}.Em {mes}você realizou uma compra no valor de R${valor} e ganhou um desconto de %{desconto} em sua proxima compra.Use o cumpom {cupom}")
