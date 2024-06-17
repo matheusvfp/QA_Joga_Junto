9275dc1fad2a2ecb92342ad516ccf3656edc8c62
Matheus Vinicius <matheusv83200@gmail.com>
2024-06-17 18:18:40 -0300

    atividade 1

--- atv1.py
+++ atv1.py
@@ -2,4 +2,4 @@ nome = input("digite seu nome: ")
 altura = float(input("digite sua altura: "))
 idade = int(input("digite sua idade: "))
 
-print(f"O nome é {nome}, tem {altura} de altura e idade de {idade} anos!")
+print(f"{nome} tem {altura} de altura e idade de {idade} anos!!!")
