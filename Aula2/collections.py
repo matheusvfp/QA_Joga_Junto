48a645293f0ee433d605c30b24e31acbe59cbb72
Matheus Vinicius <matheusv83200@gmail.com>
2024-06-17 22:01:44 -0300

    collections aula

---
+++ Aula2/collections.py
@@ -0,0 +1,20 @@
+#lista 
+frutas = ['Manga','Maracuja','Uva']
+
+frutas.append("Melancia")
+print(frutas)
+
+#Tupla
+comidas = ('hamburguer', 'Pizza', 'Feijoada')
+print(comidas)
+
+# set
+facilitadores = {"vini", "Matheus", "Michelle"}
+print(facilitadores)
+
+dicionario = {
+    "nome": "Vini",
+    "idade": 25
+}
+
+print(dicionario)
