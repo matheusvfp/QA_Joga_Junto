fbcfc11d8ca610a52524b84d9bc496696b16ac4b
Matheus Vinicius <matheusv83200@gmail.com>
2024-06-19 19:59:27 -0300

    refactoring Calculadora

--- Aula4/op_calculadora.py
+++ Aula4/op_calculadora.py
@@ -1,26 +1,28 @@
 def add(x, y):
     return x + y
 
-def subtract(x, y):
+def sub(x, y):
     return x - y
 
-def multiply(x, y):
+def mult(x, y):
     return x * y
 
-def divide(x, y):
+def div(x, y):
     if y == 0:
         return "Erro! Divisão por zero."
     return x / y
+
+
 
 def op(operacao, num1, num2):
     match operacao:
         case 1:
             return add(num1, num2)
         case 2:
-            return subtract(num1, num2)
+            return sub(num1, num2)
         case 3:
-            return multiply(num1, num2)
+            return mult(num1, num2)
         case 4:
-            return divide(num1, num2)
+            return div(num1, num2)
         case _:
             return "Operação inválida!"
