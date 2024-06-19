9c46601a9b7bb87141bb13374c7a6acb1fd5ec43
Matheus Vinicius <matheusv83200@gmail.com>
2024-06-18 21:24:28 -0300

    atv7 freq

---
+++ Aula3/atv7.py
@@ -0,0 +1,8 @@
+print("VOCÊ ESTÁ PARTICIPANDO DA NOSSA PROMO TREINA JUNTO")
+freq = int(input("digite a sua frequência: "))
+faltas = int(input("Quantos dias você faltou: "))
+
+if (freq >= 21 and faltas == 0):
+   print("UHUU. AGORA VOCÊ PODE PRESENTEAR UM AMIGO OU AMIGA PARA TREINAR COM VOCÊ")
+else:
+   print("QUE BOM VER VOCÊ DE VOLTA. A PARTIR DE AGORA INICIAMOS MAIS UMA CONTAGEM DE 21 DIAS PARA A PROMO TREINA JUNTO.")
